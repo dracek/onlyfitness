3c2b0d3e689dd75b7355ec6908bf1c239b627b4d
Artem <bevzenko@seznam.cz>
2023-11-30 21:16:21 +0100

    helloworld

--- uu_onlyfitness_maing01-server/app/api/controllers/activity-controller.js
+++ uu_onlyfitness_maing01-server/app/api/controllers/activity-controller.js
@@ -1,28 +1,15 @@
 "use strict";
-//const ActivityAbl = require("../../abl/activity-abl.js");
-const ListAbl = require("../../abl/activity/list-abl");
-const GetAbl = require("../../abl/activity/get-abl");
-const CreateAbl = require("../../abl/activity/create-abl");
 
-/*class ActivityController {
-  create(ucEnv) {
-    const awid = ucEnv.getUri().getAwid();
-    const dtoIn = ucEnv.parameters;
-    return ActivityAbl.create(awid, dtoIn);
-  }
-}*/
 class ActivityController {
 
-  static list(ucEnv) {
-    return ListAbl.list(ucEnv.getUri().getAwid(), ucEnv.parameters, ucEnv.getAuthorizationResult());
-  }
+helloWorld() {
+  return {
+    text: "Hello World!",
+    uuAppErrorMap: {},
 
-  static get(ucEnv) {
-    return GetAbl.get(ucEnv.getUri().getAwid(), ucEnv.parameters, ucEnv.getAuthorizationResult());
-  }
-  
-  static create(ucEnv) {
-    return CreateAbl.create(ucEnv.getUri().getAwid(), ucEnv.parameters, ucEnv.session, ucEnv.getAuthorizationResult());
   }
 }
-module.exports = ActivityController();
+
+}
+
+module.exports = new ActivityController();
