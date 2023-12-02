ef9088567f2d0567373578267d7330e0f17b82f2
Artem <bevzenko@seznam.cz>
2023-12-02 11:57:04 +0100

    cmd create persistence Artem

--- uu_onlyfitness_maing01-server/app/abl/category-abl.js
+++ uu_onlyfitness_maing01-server/app/abl/category-abl.js
@@ -1,6 +1,7 @@
 
 const {Validator} = require("uu_appg01_server").Validation;
 const {ValidationHelper} = require("uu_appg01_server").AppServer;
+const {DaoFactory, ObjectStoreError} = require("uu_appg01_server").ObjectStore;
 
 const Errors = require("../api/errors/category-error.js");
 
@@ -13,13 +14,29 @@ const WARNINGS = {
 class CategoryAbl {
     constructor() {
         this.validator = Validator.load();
+        this.dao = DaoFactory.getDao("category");
     }
 
-create(awid, dtoIn) {
+async create(awid, dtoIn) {
     let validationResult = this.validator.validate("categoryCreateDtoInType", dtoIn);
     let uuAppErrorMap = ValidationHelper.processValidationResult(dtoIn, validationResult, WARNINGS.createUnsupportedKeys.code, Errors.Create.invalidDtoIn)
 
-    let dtoOut = { ...dtoIn, awid, uuAppErrorMap }
+
+dtoIn.awid = awid;    
+let dtoOut;
+try {
+    dtoOut = await this.dao.create(dtoIn);
+} catch (e) {
+    if (e instanceof ObjectStoreError) {
+        throw new Errors.Create.CategoryDaoCreateFailed({ uuAppErrorMap }, e)
+    }
+
+    throw e;
+}
+
+
+
+    dtoOut.uuAppErrorMap = uuAppErrorMap;
     return dtoOut;
 }
 
