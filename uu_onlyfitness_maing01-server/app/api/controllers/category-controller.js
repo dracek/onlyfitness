f1f385d4d4cf476b8f2a9cbfcba2fa585336c13c
Artem <bevzenko@seznam.cz>
2023-12-02 16:17:39 +0100

    delete helloWorld from CategoryCreate

--- uu_onlyfitness_maing01-server/app/api/controllers/category-controller.js
+++ uu_onlyfitness_maing01-server/app/api/controllers/category-controller.js
@@ -4,13 +4,6 @@ const CategoryAbl = require("../../abl/category-abl")
 
 class CategoryController {
 
-helloWorld(ucEnv) {
-    return {
-        text: "Hello World!",
-        uuAppErrorMap: {},
-    }
-}
-
 create(ucEnv) {
     let dtoIn = ucEnv.getDtoIn();
     let awid = ucEnv.getUri().getAwid();
