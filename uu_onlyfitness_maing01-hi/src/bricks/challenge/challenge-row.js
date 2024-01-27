54765ec7b8056c0d987d1cb693081066aa429a89
Emilka <emilka.@MacBook-Air.local>
2024-01-27 13:55:37 +0100

    challenges

--- uu_onlyfitness_maing01-hi/src/bricks/challenge/challenge-row.js
+++ uu_onlyfitness_maing01-hi/src/bricks/challenge/challenge-row.js
@@ -5,6 +5,29 @@ import Config from "../config/config.js";
 import ChallengeContext from "./challenge-context";
 
 const Css = {
+  row: () => 
+    Config.Css.css({
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      alignItems: 'center', 
+      borderRadius: '30px',
+      padding: '10px 15px 0px 15px',
+      boxShadow: '0 2px 4px rgba(255, 255, 255, 0.2)',
+      outline: 'none', 
+      '&:focus': {
+        boxShadow: '0 0 5px rgba(255, 255, 255, 0.4)',
+      }
+    }),
+    buttonsContainer: () => 
+    Config.Css.css({
+      display: 'flex',
+      flexDirection: 'row',
+    }),
+  button: () => 
+    Config.Css.css({
+      color: "orange",
+      marginLeft: '10px',
+    }),
   editButton: () =>
     Config.Css.css({
       marginLeft: "10px",
@@ -15,6 +38,10 @@ const Css = {
     Config.Css.css({
       marginLeft: "10px",
       color: "orange"
+    }),
+    text: () => 
+    Config.Css.css({
+     
     }),
 };
 
@@ -29,15 +56,21 @@ const ChallengeRow = (props) => {
     }
 
     return (
-      <div>{convertCat(data.categoryId)} {data.value} hours 
-        <Button className={Css.editButton()} onClick={() => props.onEdit(data)}>
-          <Icon icon="mdi-pencil" />
-        </Button>
-        <Button className={Css.deleteButton()} onClick={() => props.onDelete(data.id)}>
-          <Icon icon="mdi-trash-can" />
-        </Button>
+      <div className={Css.row()}>
+        <div className={Css.text()}>
+          {convertCat(data.categoryId)} {data.value} hours
+        </div>
+        <div className={Css.buttonsContainer()}>
+          <Button className={Css.button()} onClick={() => props.onEdit(data)}>
+            <Icon icon="mdi-pencil" />
+          </Button>
+          <Button className={Css.button()} onClick={() => props.onDelete(data.id)}>
+            <Icon icon="mdi-trash-can" />
+          </Button>
+        </div>
       </div>
     );
-  };
+};
+
   
   export default ChallengeRow;
